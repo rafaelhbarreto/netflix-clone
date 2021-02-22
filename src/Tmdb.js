d47f2481ea88e4368d7fea8a03cfb3b41beaf910
rafael.barreto <rafael.barreto@go.senac.br>
2021-02-22 14:13:44 -0300

    Cleaning the project and tmdb config

---
+++ src/Tmdb.js
@@ -0,0 +1,27 @@
+/**
+ * Arquivo de configuração e dados da API do Tmdb
+ */
+
+const API_KEY = '9e45867a6a5b31e63c07219095fec594';
+const API_BASE = 'https://api.themoviedb.org/3'; 
+
+/**
+ * 
+ * Originais da Netflix 
+ * recomendados (trending)
+ * em alta (top rated)
+ * ação
+ * comédia
+ * terror
+ * romance
+ * documentários
+ * 
+ */
+
+export default {
+    getHomeList: async () => { 
+        return [ 
+            
+        ];
+    }    
+}
